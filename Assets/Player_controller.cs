2d0b516570a5f9c24673e084fb54363a1e97a330
Eduardo Torres Martínez <lalito.eduardo.tm@gmail.com>
2021-05-06 13:22:16 -0500

    flip pj

--- Assets/Player_controller.cs
+++ Assets/Player_controller.cs
@@ -29,12 +29,23 @@ public class Player_controller : MonoBehaviour
 
     void FixedUpdate()
     {
+
         float h = Input.GetAxis("Horizontal");
 
         rb2d.AddForce(Vector2.right * speed * h);
 
         float limitedSpeed = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
         rb2d.velocity = new Vector2(limitedSpeed, rb2d.velocity.y);
+
+
+        if (h > 0.1f){
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        if(h < -0.1f){
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
 
         Debug.Log(rb2d.velocity.x);
 
