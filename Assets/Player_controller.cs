57db4cad1a1521af9b40b1da3360b8fee662aa77
Eduardo Torres Martínez <lalito.eduardo.tm@gmail.com>
2021-05-06 08:33:29 -0500

    all files needed to run 0.1% of the game
    all files needed to run 0.1% of the game

---
+++ Assets/Player_controller.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_controller : MonoBehaviour
+{
+    public float maxSpeed = 5f;
+    public float speed = 2f;
+    public bool grounded;
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
+        anim.SetBool("Grounded", grounded);
+    }
+
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        rb2d.AddForce(Vector2.right * speed * h);
+
+        float limitedSpeed = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
+        rb2d.velocity = new Vector2(limitedSpeed, rb2d.velocity.y);
+
+        Debug.Log(rb2d.velocity.x);
+
+    }
+}
